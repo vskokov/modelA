9872537e9bca31810ff1a840155d3265e57a40cf
vskokov <vladi@skokov.net>
2022-04-19 15:41:08 -0400

    first commit

---
+++ modelA.jl
@@ -0,0 +1,139 @@
+cd(@__DIR__)
+#using Pkg
+#Pkg.activate(".")
+
+using Distributions
+using StaticArrays
+using Random
+using DelimitedFiles
+using SLEEF
+using JLD2
+
+#Random.seed!(parse(Int,ARGS[1]))
+
+### Lattice Size
+const L = 16 #parse(Int,ARGS[2])
+### end
+
+ξ = Normal(0.0f0, 1.0f0)
+
+### Parameters
+const λ = 4.0f0
+const Γ = 1.0f0
+const T = 1.0f0
+### end
+
+### Numerics
+const Δt = 0.04f0/Γ
+### end
+
+const Rate = Float32(sqrt(2.0*Δt*Γ))
+
+### Nearest neighbors
+NNp_a = zeros(Int16,L)
+NNm_a = zeros(Int16,L)
+for i in 1:L
+	NNp_a[i] = i + 1
+	NNm_a[i] = i - 1
+end
+NNp_a[L] = 1
+NNm_a[1] = L
+
+# for performance
+NNp=@SVector [NNp_a[i] for i in 1:L]
+NNm=@SVector [NNm_a[i] for i in 1:L]
+###
+
+function hotstart(n)
+	rand(ξ, (n, n, n))
+end
+
+function ΔH(m², ϕ, ϕt, x)
+	@inbounds ϕold = ϕ[x[1],x[2],x[3]]
+	Δϕ = ϕt - ϕold
+	Δϕ2= ϕt^2 - ϕold^2
+
+	## nearest neighbours
+	@inbounds Σnn = ϕ[ NNp[x[1]] , x[2], x[3] ] + ϕ[x[1], NNp[x[2]] ,x[3] ] + ϕ[x[1],x[2], NNp[x[3]] ]
+	@inbounds Σnn = Σnn + ϕ[ NNm[x[1]] ,x[2],x[3]] + ϕ[x[1],  NNm[x[2]] ,x[3]] + ϕ[x[1],x[2],  NNm[x[3]] ]
+
+	kinet_term =  3.0f0 * Δϕ2 - Δϕ * Σnn
+	poten_term =  0.5f0 * m² * Δϕ2 + 0.25f0 * λ * (ϕt^4-ϕold^4)
+
+	kinet_term + poten_term
+end
+
+
+function MCstep(m², ϕ, x)
+	r = rand(Float32)
+	@inbounds ϕt = ϕ[x[1],x[2],x[3]] + Rate*rand(ξ)
+	p = min(1.0f0,SLEEF.exp(-ΔH(m², ϕ, ϕt, x)))
+	if (r<=p)
+		@inbounds ϕ[x[1],x[2],x[3]] = ϕt
+	end
+end
+
+
+function sweep(m², ϕ, L)
+	Threads.@threads for i in 1:L
+		for j in 1:L
+			for k in 1:L
+				if (i+j+k)%2 == 0
+					MCstep(m², ϕ, (i,j,k))
+				end
+			end
+		end
+	end
+
+	Threads.@threads for i in 1:L
+		for j in 1:L
+			for k in 1:L
+				if (i+j+k)%2 !=0
+					MCstep(m², ϕ, (i,j,k))
+				end
+			end
+		end
+	end
+end
+
+function op(ϕ, L)
+	average = sum( ϕ )/L^3
+	average
+end
+
+
+function thermalize(m², ϕ, L,  N=10000)
+	for t in 1:N
+		sweep(m², ϕ, L)
+	end
+end
+
+
+###
+###
+### ************************************** Main ************************************
+###
+###
+
+
+
+
+## Thermalization study
+ϕ=hotstart(L)
+
+# near critical value
+m² = -2.285
+
+thermalize(m²,ϕ, L, 100*L^2)
+
+maxt = 10000*L^2
+
+open("output_$L.dat","w") do io 
+	for i in 0:maxt
+		println(io, i, " ", op(ϕ, L,)) # saving MC time and the average value of the magnetization
+		flush(stdout)
+		sweep(m², ϕ, L)
+	end
+end
+
+##
